c1292591c3aa361d3d8b15daa6c0758937bf1a19
sim <sc.sim@outlook.kr>
2023-09-25 17:11:09 +0900

    divider

--- test_math_op.py
+++ test_math_op.py
@@ -1,5 +1,5 @@
 import pytest
-from math_op import add
+from math_op import add, divide
 
 @pytest.fixture
 def sample_input():
@@ -7,3 +7,14 @@ def sample_input():
 
 def test_add(sample_input):
     assert add(sample_input[0], sample_input[1]) == 3
+
+@pytest.fixture
+def sample_divide_input():
+    return (4, 2)
+
+def test_divide(sample_divide_input):
+    assert divide(sample_divide_input[0], sample_divide_input[1]) == 2
+
+def test_divide_by_zero():
+    with pytest.raises(ValueError, match="Cannot divide by zero"):
+        divide(1, 0)
